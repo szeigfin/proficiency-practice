29283d54cafdb508623bc6a07d2f3a20da5a5a0e
szeigfin <szeigfin@bsdvt.org>
2026-05-23 16:21:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/szeigfin-proficiency-practice-lab/',
+  base: '/-proficiency-practice-lab/',
 })
